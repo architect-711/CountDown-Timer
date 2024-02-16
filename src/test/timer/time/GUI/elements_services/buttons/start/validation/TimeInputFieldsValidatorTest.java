1fcc4477729a0b3170113b2ed21704358b9d7103
Architect <jfjjjnqhqiriykkn@gmail.com>
2024-02-16 10:31:11 +0300

    test(TimeInputFieldsValidatorTest): add should throw exception checkout
    In addition to adding the test, I changed the getParameters method to
    get custom TimeInputFields.
    
    Previously the getParameters method took arguments as JTextFields, not
    as strings.

--- src/test/timer/time/GUI/elements_services/buttons/start/validation/TimeInputFieldsValidatorTest.java
+++ src/test/timer/time/GUI/elements_services/buttons/start/validation/TimeInputFieldsValidatorTest.java
@@ -1,14 +1,25 @@
 package test.timer.time.GUI.elements_services.buttons.start.validation;
 
 import constructors.TimeInputFields;
+import org.junit.jupiter.api.Test;
 import timer.GUI.elements_services.buttons.start.validation.TimeInputFieldsValidator;
+import timer.GUI.elements_services.buttons.start.validation.ValidationException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 import javax.swing.*;
 
 public class TimeInputFieldsValidatorTest {
     private TimeInputFieldsValidator validator;
 
-    private TimeInputFields getParameters(JTextField hours, JTextField minutes, JTextField seconds) {
-        return new TimeInputFields(hours, minutes, seconds);
+    @Test
+    public void shouldThrowException() {
+        validator = new TimeInputFieldsValidator(getParameters("", "", ""));
+
+        assertThrows(ValidationException.class, () -> validator.validate());
+    }
+
+    private TimeInputFields getParameters(String hours, String minutes, String seconds) {
+        return new TimeInputFields(new JTextField(hours), new JTextField(minutes), new JTextField(seconds));
     }
 }
