7db939ace37eebff546650557f7fca37309969e9
Architect <jfjjjnqhqiriykkn@gmail.com>
2024-02-16 10:26:28 +0300

    test(TimeInputFieldsValidatorTest): add class and getParameters method

--- src/test/timer/time/GUI/elements_services/buttons/start/validation/TimeInputFieldsValidatorTest.java
+++ src/test/timer/time/GUI/elements_services/buttons/start/validation/TimeInputFieldsValidatorTest.java
@@ -1,4 +1,14 @@
 package test.timer.time.GUI.elements_services.buttons.start.validation;
 
+import constructors.TimeInputFields;
+import timer.GUI.elements_services.buttons.start.validation.TimeInputFieldsValidator;
+
+import javax.swing.*;
+
 public class TimeInputFieldsValidatorTest {
+    private TimeInputFieldsValidator validator;
+
+    private TimeInputFields getParameters(JTextField hours, JTextField minutes, JTextField seconds) {
+        return new TimeInputFields(hours, minutes, seconds);
+    }
 }
