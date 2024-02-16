f9f22263f740a873fc4ed0b6644e2251831f9e61
Architect <jfjjjnqhqiriykkn@gmail.com>
2024-02-16 08:39:46 +0300

    feat(PrettyTimeTest): add getParameters method

--- src/test/timer/time/PrettyTimeTest.java
+++ src/test/timer/time/PrettyTimeTest.java
@@ -1,5 +1,12 @@
 package test.timer.time;
 
+import constructors.TimeInputFields;
+
+import javax.swing.*;
+
 class PrettyTimeTest {
 
+    private TimeInputFields getParameters(String hours, String minutes, String seconds) {
+        return new TimeInputFields(new JTextField(hours), new JTextField(minutes), new JTextField(seconds));
+    }
 }
