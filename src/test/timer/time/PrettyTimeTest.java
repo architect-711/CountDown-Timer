a5242af48d60bf8d23242d674b59207f971f7b7a
Architect <jfjjjnqhqiriykkn@gmail.com>
2024-02-16 09:35:05 +0300

    test(PrettyTimeTest): add should return hour
    PrettyTime should return 01:00:00 when given 3600 seconds.

--- src/test/timer/time/PrettyTimeTest.java
+++ src/test/timer/time/PrettyTimeTest.java
@@ -1,10 +1,29 @@
 package test.timer.time;
 
 import constructors.TimeInputFields;
+import org.junit.jupiter.api.Test;
+import timer.time.PrettyTime;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 import javax.swing.*;
 
 class PrettyTimeTest {
+    private PrettyTime prettyTime;
+
+    @Test
+    public void shouldNotThrowException() {
+        prettyTime = new PrettyTime(0);
+
+        assertDoesNotThrow(() -> prettyTime.prepareTimeToView());
+    }
+
+    @Test
+    public void shouldReturnHour() {
+        prettyTime = new PrettyTime(3600);
+
+        assertEquals(prettyTime.getPrettyTime(), "01:00:00");
+    }
 
     private TimeInputFields getParameters(String hours, String minutes, String seconds) {
         return new TimeInputFields(new JTextField(hours), new JTextField(minutes), new JTextField(seconds));
